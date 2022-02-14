4f608185b105a5fa46620a7b97056742bbb72548
rahul-netizen <rahulkumar260100@gmail.com>
2022-02-14 23:23:00 +0530

    chrome driver options

--- scrapper.py
+++ scrapper.py
@@ -134,7 +134,7 @@ def start_driver():
 
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
 
