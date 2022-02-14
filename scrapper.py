69968f36ff18c4a7a3a1e43e7af3886c8954e5c7
rahul-netizen <rahulkumar260100@gmail.com>
2022-02-14 23:14:33 +0530

    chrome driver options

--- scrapper.py
+++ scrapper.py
@@ -127,9 +127,16 @@ def start_driver():
     # initializing the chrome driver
     print('Starting chrome web driver')
 
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(
-        executable_path='./chromedriver.exe', options=options)
+    # options = webdriver.ChromeOptions()
+    # driver = webdriver.Chrome(
+    #     executable_path='./chromedriver.exe', options=options)
+
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+
     driver.get('https://store.steampowered.com/')  # opening browswer
 
     driver.maximize_window()  # maximize to view top sellers options
