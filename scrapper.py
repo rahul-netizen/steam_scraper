eb4ddf85b28268ca5f46746d61763b293e0a5d24
rahul-netizen <rahulkumar260100@gmail.com>
2022-02-14 16:29:35 +0530

    Inceased titles

--- scrapper.py
+++ scrapper.py
@@ -219,7 +219,7 @@ def scrape_steam_page(num):
 
 
 if __name__ == '__main__':
-    num_titles = 50
+    num_titles = 500
     game_info = scrape_steam_page(num_titles)
     # print(game_info[:5])
     send_email
