7145b2179e0bc7f134955abb196830cca1b55730
rahul-netizen <rahulkumar260100@gmail.com>
2022-02-15 15:00:33 +0530

    linux

--- scrapper.py
+++ scrapper.py
@@ -141,7 +141,7 @@ def start_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     #chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get('https://store.steampowered.com/')  # opening browswer
