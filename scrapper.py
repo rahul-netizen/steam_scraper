41b845d5e321301adee53ce7123b0a199e1a33a6
rahul-netizen <rahulkumar260100@gmail.com>
2022-02-15 15:17:32 +0530

    linux fix

--- scrapper.py
+++ scrapper.py
@@ -139,10 +139,10 @@ def start_driver():
     print(os.getcwd())
 
     chrome_options = Options()
-    #chrome_options.add_argument('--no-sandbox')
     #chrome_options.add_argument('--headless')
-    #chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get('https://store.steampowered.com/')  # opening browswer
